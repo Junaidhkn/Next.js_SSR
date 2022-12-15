f60cc07213220b4fc7bf4c21955ea6036a017066
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-16 00:18:54 +0500

    finished routes

--- 2-project-file_based_routing/pages/index.js
+++ 2-project-file_based_routing/pages/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import EventList from '../components/events/EventList.js';
-import { getFeaturedEvents } from '../dummy-data.js';
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
 
-const Home = () => {
-	const featuredEvents = getFeaturedEvents();
-	return (
-		<div>
-			<h1>Home Page</h1>
-			<EventList events={featuredEvents} />
-		</div>
-	);
-};
+function HomePage() {
+  const featuredEvents = getFeaturedEvents();
 
-export default Home;
+  return (
+    <div>
+      <EventList items={featuredEvents} />
+    </div>
+  );
+}
+
+export default HomePage;
