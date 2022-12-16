81f81caaff62fda8827e8d245d82882383d63c84
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-17 00:10:08 +0500

    commit

--- 3-pre_rendering-data_fetching/pages/index.js
+++ 3-pre_rendering-data_fetching/pages/index.js
@@ -13,7 +13,7 @@ const HomePage = (props) => {
 	);
 };
 
-export const getStaticProps = async () => {
+export const getStaticProps = async (context) => {
 	const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
 	const jsonData = await fs.readFile(filePath);
 	const data = JSON.parse(jsonData);
