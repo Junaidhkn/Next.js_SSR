cc9c20fe49e4f30cb38384bd8f86f338e8f799c6
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-16 22:09:24 +0500

    commit

--- 3-pre_rendering-data_fetching/pages/index.js
+++ 3-pre_rendering-data_fetching/pages/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 
-const HomePage = () => {
+const HomePage = (props) => {
+	const { products } = props;
 	return (
-		<div>
-			<h1>Home Page</h1>
-		</div>
+		<ul>
+			{products.map((product) => {
+				return <li key={product.id}>{product.title}</li>;
+			})}
+		</ul>
 	);
+};
+
+export const getStaticProps = async () => {
+	return {
+		props: {
+			products: [{ id: 'p1', title: 'Product One' }],
+		},
+	};
 };
 
 export default HomePage;
