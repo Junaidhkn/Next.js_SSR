2123014195c2ca164ccfa393b5743b96a3e7d171
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-17 22:34:53 +0500

    Server Side Props

--- 4-project-pre_rendering-data_fetching/pages/index.js
+++ 4-project-pre_rendering-data_fetching/pages/index.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const HopePage = () => {
+const HopePage = ( props ) => {
   return (
-    <div>HopePage</div>
+    <div>
+      <h1>{props.username}</h1>
+      <p>{props.desc}</p>
+    </div>
   )
 }
 
+
+
 export default HopePage
+
+
+export const getServerSideProps = async () => {
+  return {
+    props: {
+      username: 'Junaid',
+      desc: 'ewerw erwerwer werwerwer wer wer eriwerkjlwer weirwe rniwjer  ojweklrwer wnerwer erwerwer werwerwer werwerwer efrgfeg3fgg rgrgfwefgg werwefwee werfwerwer etrhojsldf'
+    }
+  }
+}
