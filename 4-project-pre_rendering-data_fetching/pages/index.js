98d69c2b81766abf46a19d091d9380ea02e3f23b
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-18 23:09:23 +0500

    commit

--- 4-project-pre_rendering-data_fetching/pages/index.js
+++ 4-project-pre_rendering-data_fetching/pages/index.js
@@ -1,13 +1,14 @@
-import React from 'react'
+import { getFeaturedEvents } from '../dummy-data';
+import EventList from '../components/events/event-list';
 
-const HomePage = () => {
+function HomePage() {
+  const featuredEvents = getFeaturedEvents();
+
   return (
     <div>
-      <h1>Junaid Hassan khan</h1>
+      <EventList items={featuredEvents} />
     </div>
-  )
+  );
 }
 
-
-
-export default HomePage
+export default HomePage;
