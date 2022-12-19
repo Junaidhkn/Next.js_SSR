d48db0e746292de251d31201835ac847ab943a01
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-20 01:08:36 +0500

    commit

---
+++ 4-project-pre_rendering-data_fetching/helpers/api-utils.js
@@ -0,0 +1,20 @@
+export const getAllEvents = async () => {
+   const response = await fetch( 'https://nextjs-project-pre-rendering-default-rtdb.asia-southeast1.firebasedatabase.app/events' )
+
+   const data = await response.json()
+
+   const events = []
+
+   for ( const key in data ) {
+      events.push( { id: key, ...data[key] } )
+   }
+   return events
+
+}
+
+
+export const getFeaturedEvents = async () => {
+   const allEvents = await getAllEvents()
+   const featuredEvents = allEvents.filter( ( event ) => event.isFeatured );
+   return featuredEvents
+}
--- 4-project-pre_rendering-data_fetching/pages/index.js
+++ 4-project-pre_rendering-data_fetching/pages/index.js
@@ -1,14 +1,21 @@
-import { getFeaturedEvents } from '../dummy-data';
+import { getFeaturedEvents } from '../helpers/api-utils';
 import EventList from '../components/events/event-list';
 
-function HomePage() {
-  const featuredEvents = getFeaturedEvents();
-
-  return (
-    <div>
-      <EventList items={featuredEvents} />
-    </div>
-  );
+function HomePage(props) {
+	return (
+		<div>
+			<EventList items={props.Events} />
+		</div>
+	);
 }
 
 export default HomePage;
+
+export const getStaticProps = async () => {
+	const featuredEvents = await getFeaturedEvents();
+	return {
+		props: {
+			Events: featuredEvents,
+		},
+	};
+};
