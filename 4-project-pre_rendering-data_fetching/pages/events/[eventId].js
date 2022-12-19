3adb8568d6da5be4e2c9631feb585ef97fe6b2a0
Junaidhkn <junaidhkniazi@gmail.com>
2022-12-20 02:15:22 +0500

    added static generation

--- 4-project-pre_rendering-data_fetching/pages/events/[eventId].js
+++ 4-project-pre_rendering-data_fetching/pages/events/[eventId].js
@@ -1,40 +1,47 @@
 import { Fragment } from 'react';
 import { useRouter } from 'next/router';
 
-import { getEventById } from '../../dummy-data';
+import { getEventById } from '../../helpers/api-utils.js';
 import EventSummary from '../../components/event-detail/event-summary';
 import EventLogistics from '../../components/event-detail/event-logistics';
 import EventContent from '../../components/event-detail/event-content';
 import ErrorAlert from '../../components/ui/error-alert';
 
-function EventDetailPage() {
-  const router = useRouter();
+function EventDetailPage(props) {
+	const event = props.eventDetail;
 
-  const eventId = router.query.eventId;
-  const event = getEventById(eventId);
+	if (!event) {
+		return (
+			<ErrorAlert>
+				<p>No event found!</p>
+			</ErrorAlert>
+		);
+	}
 
-  if (!event) {
-    return (
-      <ErrorAlert>
-        <p>No event found!</p>
-      </ErrorAlert>
-    );
-  }
-
-  return (
-    <Fragment>
-      <EventSummary title={event.title} />
-      <EventLogistics
-        date={event.date}
-        address={event.location}
-        image={event.image}
-        imageAlt={event.title}
-      />
-      <EventContent>
-        <p>{event.description}</p>
-      </EventContent>
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<EventSummary title={event.title} />
+			<EventLogistics
+				date={event.date}
+				address={event.location}
+				image={event.image}
+				imageAlt={event.title}
+			/>
+			<EventContent>
+				<p>{event.description}</p>
+			</EventContent>
+		</Fragment>
+	);
 }
 
 export default EventDetailPage;
+
+export const getStaticProps = async (context) => {
+	const eventId = context.params.eventId;
+	const event = await getEventById(eventId);
+	return {
+		props: {
+			eventDetail: event,
+		},
+	};
+};
